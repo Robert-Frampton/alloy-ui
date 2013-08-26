cf4612bca32fb6ab7884c630925eda2a014fd33f
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2013-08-26 14:25:57 -0700

    AUI-974 Buttons generated in aui-toolbar should default to 'button' type

--- src/aui-toolbar/HISTORY.md
+++ src/aui-toolbar/HISTORY.md
@@ -5,3 +5,4 @@ AUI Toolbar
 ------
 
 	* #AUI-971 - Set title attribute to button node during the process of creation.
+	* #AUI-974 - Configure type attribute of button nodes to defult to 'button'
--- src/aui-toolbar/tests/unit/js/tests.js
+++ src/aui-toolbar/tests/unit/js/tests.js
@@ -7,15 +7,20 @@ YUI.add('module-tests', function(Y) {
     var suite = new Y.Test.Suite('aui-toolbar');
 
     //--------------------------------------------------------------------------
-    // Test Case for setting title to button node
+    // Test Case for Toolbar Button Nodes
     //--------------------------------------------------------------------------
 
     suite.add(new Y.Test.Case({
 
         name: 'AUI Toolbar Render Button Tests',
 
+        //----------------------------------------------------------------------
+        // Tests
+        //----------------------------------------------------------------------
+
         /*
          * Check if title attribute is set during the process of creation
+         *
          * @tests AUI-971
          */
         'test setting title attribute': function() {
@@ -33,10 +38,27 @@ YUI.add('module-tests', function(Y) {
             }).render();
 
             Y.Assert.isNotNull(Y.one('#toolbar').one('button[title="' + title + '"]'), 'Title attribute should be set to the button node.');
-        }
+        },
+
+        /*
+         * Check if type attribute is defaulting to 'button' during the process of creation
+         *
+         * @tests AUI-974
+         */
+         'test default type attribute': function() {
+            var toolbar;
+
+            toolbar = new Y.Toolbar({
+                boundingBox: '#toolbar',
+                children: [{
+                    label: 'Test label'
+                }]
+            }).render();
+
+            Y.Assert.isNotNull(Y.one('#toolbar').one('button[type="button"]'), 'Type attribute of the button node should default to button');
+         }
     }));
 
     Y.Test.Runner.add(suite);
-
 
 },'', { requires: [ 'test', 'aui-toolbar' ] });
